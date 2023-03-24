40384e3885d71b8db1ee52a1109d061ed8aa4606
Geert Meersman <geertmeersman@gmail.com>
2023-03-24 16:33:54 +0100

    manifest correction

--- custom_components/telenet/__init__.py
+++ custom_components/telenet/__init__.py
@@ -1,33 +1,30 @@
-import logging
 import json
+import logging
 from pathlib import Path
+
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+
 from .utils import *
-from homeassistant.const import (
-    Platform,
-    CONF_PASSWORD,
-    CONF_USERNAME
-)
 
 PLATFORMS = [Platform.SENSOR]
 _LOGGER = logging.getLogger(__name__)
 DATETIME_FORMAT = '%Y-%m-%d'
 UPDATE_INTERVAL = timedelta(minutes=15)
 CONNECTION_RETRY = 2
+WEBSITE = "https://mijn.telenet.be/mijntelenet/"
 
 manifestfile = Path(__file__).parent / 'manifest.json'
-with open(manifestfile, 'r') as json_file:
+with open(manifestfile) as json_file:
     manifest_data = json.load(json_file)
     
 DOMAIN = manifest_data.get("domain")
 NAME = manifest_data.get("name")
 VERSION = manifest_data.get("version")
 ISSUEURL = manifest_data.get("issue_tracker")
-WEBSITE = manifest_data.get("website")
-
 STARTUP = """
 -------------------------------------------------------------------
 {name}
